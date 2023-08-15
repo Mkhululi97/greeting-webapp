e570b0bf11f96f747de6acf88721b49a309035eb
mkhululi <mkhululimtshali@gmail.com>
2023-08-15 14:30:22 +0200

    added changes to the yml file

--- database.js
+++ database.js
@@ -5,6 +5,7 @@ const connectDb = {
   user: process.env.PGUSER,
   host: process.env.PGHOST,
   database: process.env.PGDATABASE,
+  database_url: process.env.PGDATABASE_URL,
   password: process.env.PGPASSWORD,
   port: process.env.PGPORT,
   ssl: true,
