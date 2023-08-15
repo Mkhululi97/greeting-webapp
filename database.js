5a8efa2e870338c8fbca285ec32e1755cb18a2e2
mkhululi <mkhululimtshali@gmail.com>
2023-08-15 15:43:39 +0200

    added changes to my database.js file

--- database.js
+++ database.js
@@ -5,11 +5,11 @@ const connectDb = {
   user: process.env.PGUSER,
   host: process.env.PGHOST,
   database: process.env.PGDATABASE,
-  database_url: process.env.PGDATABASE_URL,
   password: process.env.PGPASSWORD,
   port: process.env.PGPORT,
   ssl: true,
 };
 
 const db = pgPromise()(connectDb);
+db.connect();
 export default db;
