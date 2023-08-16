763abfe2bdb48b5354650434824840941375e6ec
mkhululi <mkhululimtshali@gmail.com>
2023-08-16 09:33:25 +0200

    add file changes

--- database.js
+++ database.js
@@ -1,16 +1,22 @@
 import pgPromise from "pg-promise";
 import dotenv from "dotenv";
 dotenv.config();
+
 const connectDb = {
   // user: process.env.PGUSER,
   // host: process.env.PGHOST,
   // database: process.env.PGDATABASE,
   // password: process.env.PGPASSWORD,
   // port: process.env.PGPORT,
+  // ssl: true,
   databaseurl: process.env.PGDATABASE_URL,
-  ssl: true,
 };
-
 const db = pgPromise()(connectDb);
+// console.log(db.connect());
+if (process.env.PGDATABASE_URL) {
+  console.log("DATABASE_URL is set:");
+} else {
+  console.log("DATABASE_URL is not set.");
+}
 db.connect();
 export default db;
