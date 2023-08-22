b14c3787b7025a3104654aa892559ae6cb531dd9
mkhululi <mkhululimtshali@gmail.com>
2023-08-22 11:35:12 +0200

    worked on mentors feedback

--- public/frontendjs/index.js
+++ public/frontendjs/index.js
@@ -1,12 +1,21 @@
 /* ##### GET P ELEMENT THAT DISPLAYS ERROR TEXTS ##### */
-let errorTextEle = document.querySelector(".error-text");
+const errorTextEle = document.querySelector(".error-text");
 /* ##### GET P ELEMENT THAT DISPLAYS WHEN DATABASE IS CLEARED ##### */
-let clearDbTextEle = document.querySelector(".db-text");
+const clearDbTextEle = document.querySelector(".db-text");
 /* ##### GET P ELEMENT THAT DISPLAYS GREETINGS TEXTS ##### */
-let greetTextEle = document.querySelector(".output-str");
+const greetTextEle = document.querySelector(".output-str");
+/* ##### GET P ELEMENT THAT DISPLAYS GREETINGS TEXTS ##### */
+const resetBtn = document.querySelector(".reset-btn");
 /* CLEAR ALL OUT MESSAGES AFTER BEING DISPLAYED */
 setTimeout(function () {
   errorTextEle.innerHTML !== "" ? (errorTextEle.innerHTML = "") : "";
   clearDbTextEle.innerHTML !== "" ? (clearDbTextEle.innerHTML = "") : "";
   greetTextEle.innerHTML !== "" ? (greetTextEle.innerHTML = "") : "";
 }, 2500);
+resetBtn.addEventListener("click", function (e) {
+  const deleteData = window.confirm(
+    "Are you sure you want to reset the counter"
+  );
+  //when true delete the data, when false do not delete data
+  deleteData ? "" : e.preventDefault();
+});
